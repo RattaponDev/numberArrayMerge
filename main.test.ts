64dc084edd7256a9390a902339a8582f1aa4bb44
rattapon umken <160044521+RattaponDev@users.noreply.github.com>
2025-04-16 20:13:58 +0700

    Update main.test.ts

--- main.test.ts
+++ main.test.ts
@@ -1,20 +1,20 @@
 
-const a = require('./main')
+const mergeFunction = require('./main')
 describe('merge', () => {
   it('should merge and sort correctly', () => {
-    expect(a([1, 3, 5], [6, 4, 2], [0, 7, 8])).toEqual([0, 1, 2, 3, 4, 5, 6, 7, 8]);
+    expect(mergeFunction([1, 3, 5], [6, 4, 2], [0, 7, 8])).toEqual([0, 1, 2, 3, 4, 5, 6, 7, 8]);
+    expect(mergeFunction([1, 3], [6,5, 4, 2], [0, 7, 8])).toEqual([0, 1, 2, 3, 4, 5, 6, 7, 8]);
+  });
+  it('should work with negative numbers', () => {
+    expect(mergeFunction([-3, -1], [0, -2,-6], [-5, -4])).toEqual([-6, -5, -4, -3, -2, -1, 0]);
   });
 
   it('should handle empty arrays', () => {
-    expect(a([], [], [])).toEqual([]);
-    expect(a([], [3, 2, 1], [])).toEqual([1, 2, 3]);
+    expect(mergeFunction([], [], [])).toEqual([]);
+    expect(mergeFunction([], [3, 2, 1], [])).toEqual([1, 2, 3]);
+    expect(mergeFunction([], [3, 2, 1], [0])).toEqual([0,1, 2, 3]);
   });
-
   it('should handle duplicates', () => {
-    expect(a([1, 2], [2, 1], [1, 2])).toEqual([1, 1, 1, 2, 2, 2]);
-  });
-
-  it('should work with negative numbers', () => {
-    expect(a([-3, -1], [0, -2], [-5, -4])).toEqual([-5, -4, -3, -2, -1, 0]);
+    expect(mergeFunction([1,2,3], [3,2,1], [1, 2,3])).toEqual([1,1,1,2,2,2,3,3,3]);
   });
 });
